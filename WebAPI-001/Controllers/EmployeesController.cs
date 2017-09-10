c2ce52cc61678a844c4a65aaec27b24d8dbf3beb
Alan Jui <alanjui.1960@gmail.com>
2017-09-10 10:04:12 +0800

    為配合DevExtreme在CustomStore的特性，改變ＡＰＩ寫法

--- WebAPI-001/Controllers/EmployeesController.cs
+++ WebAPI-001/Controllers/EmployeesController.cs
@@ -11,8 +11,8 @@ using WebAPI_001.Models;
 
 namespace WebAPI_001.Controllers
 {
-    //[Produces("application/json")]
-    [Route("api/employees")]
+    [Produces("application/json")]
+    [Route("api")]
     public class EmployeesController : Controller
     {
         private readonly NorthwindContext _context;
@@ -22,19 +22,61 @@ namespace WebAPI_001.Controllers
             _context = context;
         }
 
+        [HttpGet("employees-list")]
+        public object GetList(DataSourceLoadOptions loadOptions)
+        {
+            return DataSourceLoader.Load(_context.Employees, loadOptions);
+        }
+
+        [HttpPost("employees-create")]
+        public IActionResult CreateRecord(string values)
+        {
+            var record = new Employee();
+            JsonConvert.PopulateObject(values, record);
+
+            if (!TryValidateModel(record))
+                return BadRequest(ModelState.ToFullErrorString());
+            _context.Employees.Add(record);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPut("employees-update")]
+        public IActionResult UpdateRecord(int key, string values)
+        {
+            var record = _context.Employees.First<Employee>(r => r.EmployeeId == key);
+            JsonConvert.PopulateObject(values, record);
+
+            if (!TryValidateModel(record))
+                return BadRequest(ModelState.ToFullErrorString());
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("employees-delete")]
+        public void DeleteRecord(int key)
+        {
+            var record = _context.Employees.First<Employee>(r => r.EmployeeId == key);
+            _context.Employees.Remove(record);
+            _context.SaveChanges();
+        }
+
+        // =========================================================================================
+        // RESTful API
+        // =========================================================================================
+
         // GET: api/Employees
-        [HttpGet]
-        //public object GetEmployees(DataSourceLoadOptions loadOptions)
-        //{
-        //    return DataSourceLoader.Load(_context.Employees, loadOptions);
-        //}
+        [HttpGet("employees")]
         public IEnumerable<Employee> GetEmployees()
         {
             return _context.Employees;
         }
 
         // GET: api/Employees/5
-        [HttpGet("{id}")]
+        [HttpGet("employees/{id}")]
         public async Task<IActionResult> GetEmployee([FromRoute] int id)
         {
             if (!ModelState.IsValid)
@@ -53,7 +95,7 @@ namespace WebAPI_001.Controllers
         }
 
         // PUT: api/Employees/5
-        [HttpPut("{id}")]
+        [HttpPut("employees/{id}")]
         public async Task<IActionResult> PutEmployee([FromRoute] int id, [FromBody] Employee employee)
         {
             if (!ModelState.IsValid)
@@ -87,8 +129,9 @@ namespace WebAPI_001.Controllers
             return NoContent();
         }
 
+
         // POST: api/Employees
-        [HttpPost]
+        [HttpPost("employees")]
         public async Task<IActionResult> PostEmployee([FromBody] Employee employee)
         {
             if (!ModelState.IsValid)
@@ -103,7 +146,7 @@ namespace WebAPI_001.Controllers
         }
 
         // DELETE: api/Employees/5
-        [HttpDelete("{id}")]
+        [HttpDelete("employees/{id}")]
         public async Task<IActionResult> DeleteEmployee([FromRoute] int id)
         {
             if (!ModelState.IsValid)
@@ -128,4 +171,120 @@ namespace WebAPI_001.Controllers
             return _context.Employees.Any(e => e.EmployeeId == id);
         }
     }
+
+
+    //[Route("api/[controller]")]
+    ////[Route("api/employee")]
+    //public class EmployeesController : Controller
+    //{
+    //    private readonly NorthwindContext _context;
+
+    //    public EmployeesController(NorthwindContext context)
+    //    {
+    //        _context = context;
+    //    }
+
+    //    // GET: api/Employees
+    //    [HttpGet]
+    //    public IEnumerable<Employee> GetEmployees()
+    //    {
+    //        return _context.Employees;
+    //    }
+
+    //    // GET: api/Employees/5
+    //    [HttpGet("{id}")]
+    //    public async Task<IActionResult> GetEmployee([FromRoute] int id)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        var employee = await _context.Employees.SingleOrDefaultAsync(m => m.EmployeeId == id);
+
+    //        if (employee == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        return Ok(employee);
+    //    }
+
+    //    // PUT: api/Employees/5
+    //    [HttpPut("{id}")]
+    //    public async Task<IActionResult> PutEmployee([FromRoute] int id, [FromBody] Employee employee)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        if (id != employee.EmployeeId)
+    //        {
+    //            return BadRequest();
+    //        }
+
+    //        _context.Entry(employee).State = EntityState.Modified;
+
+    //        try
+    //        {
+    //            await _context.SaveChangesAsync();
+    //        }
+    //        catch (DbUpdateConcurrencyException)
+    //        {
+    //            if (!EmployeeExists(id))
+    //            {
+    //                return NotFound();
+    //            }
+    //            else
+    //            {
+    //                throw;
+    //            }
+    //        }
+
+    //        return NoContent();
+    //    }
+
+
+    //    // POST: api/Employees
+    //    [HttpPost]
+    //    public async Task<IActionResult> PostEmployee([FromBody] Employee employee)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        _context.Employees.Add(employee);
+    //        await _context.SaveChangesAsync();
+
+    //        return CreatedAtAction("GetEmployee", new { id = employee.EmployeeId }, employee);
+    //    }
+
+    //    // DELETE: api/Employees/5
+    //    [HttpDelete("{id}")]
+    //    public async Task<IActionResult> DeleteEmployee([FromRoute] int id)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
+
+    //        var employee = await _context.Employees.SingleOrDefaultAsync(m => m.EmployeeId == id);
+    //        if (employee == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        _context.Employees.Remove(employee);
+    //        await _context.SaveChangesAsync();
+
+    //        return Ok(employee);
+    //    }
+
+    //    private bool EmployeeExists(int id)
+    //    {
+    //        return _context.Employees.Any(e => e.EmployeeId == id);
+    //    }
+    //}
 }
