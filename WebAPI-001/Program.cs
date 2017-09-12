5b38bb29173bb4c002e9d9a57b6700d190e831a8
Alan Jui <alanjui.1960@gmail.com>
2017-09-12 11:04:59 +0800

    引用環境變數，令伺服器可分：開發機、測試機、正式機。

--- WebAPI-001/Program.cs
+++ WebAPI-001/Program.cs
@@ -20,7 +20,7 @@ namespace WebAPI_001
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://0.0.0.0:5000")
+                //.UseUrls("http://0.0.0.0:5000")
                 .Build();
     }
 }
