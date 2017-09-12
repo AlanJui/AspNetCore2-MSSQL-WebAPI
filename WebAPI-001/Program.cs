6afb1d962c7ce55b8b6069c66341bf6cc56919dc
Alan Jui <alanjui.1960@gmail.com>
2017-09-12 20:52:13 +0800

    為 QA (Staging) 環境變更

--- WebAPI-001/Program.cs
+++ WebAPI-001/Program.cs
@@ -12,6 +12,9 @@ namespace WebAPI_001
 {
     public class Program
     {
+        public static IConfigurationRoot Configuration { get; set; }
+        private static string myURL;
+
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
@@ -19,8 +22,58 @@ namespace WebAPI_001
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    IHostingEnvironment env = builderContext.HostingEnvironment;
+
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+                    var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    Configuration = builder.Build();
+                    myURL = Configuration["API_URL"];
+                    Console.WriteLine($"myURL = {myURL}");
+                })
+                .PreferHostingUrls(false)
+                //.UseUrls(myURL)
                 .UseStartup<Startup>()
-                //.UseUrls("http://0.0.0.0:5000")
                 .Build();
+
+        public static IWebHost aBuildWebHost(string[] args)
+        {
+            return new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    IHostingEnvironment env = builderContext.HostingEnvironment;
+
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+                    var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    Configuration = builder.Build();
+                    myURL = Configuration["API_URL"];
+                    Console.WriteLine($"myURL = {myURL}");
+
+                    //if (env.IsStaging())
+                    //{
+                    //    myURL = "http://0.0.0.0:5555";
+                    //} else
+                    //{
+                    //    myURL = "http://0.0.0.0:5000";
+                    //}
+
+                })
+                .UseStartup<Startup>()
+                .UseUrls(myURL)
+                .Build();
+        }
     }
 }
